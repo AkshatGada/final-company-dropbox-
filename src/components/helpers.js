74c62ead61b030185c9e7135fa8b1a564f97444d
AkshatGada <96907514+AkshatGada@users.noreply.github.com>
2023-06-20 15:21:59 +0530

    final

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -4,3 +4,4 @@ export function convertBytes(bytes) {
    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 }
+//helper function
